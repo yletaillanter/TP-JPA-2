75adae37b0436d5b5f33a39855234d4d78182b2a
yletaillanter <y.letaillanter@gmail.com>
2015-03-01 22:59:18 +0100

    Ajout myServlet -> OK

---
+++ src/main/java/fr/istic/tpjpa/domain/SmartDevice.java
@@ -0,0 +1,48 @@
+package fr.istic.tpjpa.domain;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="DeviceType", discriminatorType = DiscriminatorType.STRING)
+public abstract class SmartDevice {
+	private Long id;
+	private int conso;
+	private Home home;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getConso() {
+		return conso;
+	}
+
+	public void setConso(int conso) {
+		this.conso = conso;
+	}
+	
+	@ManyToOne
+	public Home getHome() {
+		return home;
+	}
+
+	public void setHome(Home home) {
+		this.home = home;
+	}
+	
+}
