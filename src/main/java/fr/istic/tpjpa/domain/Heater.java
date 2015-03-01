926f9458cb26e521c0ea0515cc51ef26d6b42e5a
yletaillanter <y.letaillanter@gmail.com>
2015-03-01 17:51:33 +0100

    ajout Heater

---
+++ src/main/java/fr/istic/tpjpa/domain/Heater.java
@@ -0,0 +1,10 @@
+package fr.istic.tpjpa.domain;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Heater")
+public class Heater extends Device {
+
+}
