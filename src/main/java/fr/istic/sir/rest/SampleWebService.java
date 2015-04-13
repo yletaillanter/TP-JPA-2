0d4df19815aef9c5889c5daadef328f228386f5b
yletaillanter <y.letaillanter@gmail.com>
2015-04-13 12:09:53 +0200

    Ajout d'une méthode permettant de créer une Personne -> bug avec AngularJS form vide.

--- src/main/java/fr/istic/sir/rest/SampleWebService.java
+++ src/main/java/fr/istic/sir/rest/SampleWebService.java
@@ -3,8 +3,10 @@ package fr.istic.sir.rest;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import fr.istic.tpjpa.domain.Gender;
 import fr.istic.tpjpa.domain.Heater;
 import fr.istic.tpjpa.domain.Home;
 import fr.istic.tpjpa.domain.Person;
@@ -15,11 +17,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 @Path("/hello")
 public class SampleWebService {
@@ -92,6 +100,36 @@ public class SampleWebService {
         tx.commit();
 
         return query.getResultList();
+    }
+    
+    @POST
+    @Path("/newPerson")
+    public JSONObject newPerson(@FormParam("name") String name, @FormParam("firstName") String firstName) {
+    
+    	// test return JSON pour erreur angular
+    	JSONObject jsonObj = new JSONObject();
+    	
+    	if( !name.equals(null) &&  !firstName.equals(null)){
+    		try {
+    			jsonObj.put("name", name);
+    			jsonObj.put("firstName", firstName);
+    		} catch (JSONException e) {
+    			e.printStackTrace();
+    		}
+
+        	// Creation d l'entity manager
+        	EntityManagerFactory factory = Persistence.createEntityManagerFactory("example");
+            EntityManager manager = factory.createEntityManager();
+            EntityTransaction tx = manager.getTransaction();
+            
+        	// Creation de la nouvelle personnne
+            Person personToSave = new Person(name,firstName,Gender.male,new Date());
+            // Enregistrement dans la db
+           	tx.begin();
+           	manager.persist(personToSave);
+           	tx.commit();
+    	}
+    	return jsonObj;
     }
     
 }
